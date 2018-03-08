7bd900c5cc467767fde9a3246c10422e8e7f8ea0
Samuel C Tyler <skamansam@gmail.com>
2018-03-08 01:12:06 -0500

    add theme and toolbar for material design

--- src/NoInc/SimpleStorefront/ViewBundle/Resources/src/scripts/app.js
+++ src/NoInc/SimpleStorefront/ViewBundle/Resources/src/scripts/app.js
@@ -1,2 +1,17 @@
 // eslint-disable-next-line
-const app = angular.module('noincSimpleStorefront', ['ui.router']);
+const app = angular.module('noincSimpleStorefront', ['ui.router', 'ngMaterial'])
+    .config(($mdThemingProvider) => {
+        $mdThemingProvider.theme('default')
+            .primaryPalette('purple', {
+                default: '400',
+                'hue-1': 'A200', // use shade A200 for the <code>md-hue-1</code> class
+                'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+                'hue-3': 'A700', // use shade A100 for the <code>md-hue-3</code> class
+            })
+            .accentPalette('red', {
+                default: 'A700', // use shade 200 for default, and keep all other shades the same
+                'hue-1': '500', // use shade A200 for the <code>md-hue-1</code> class
+                'hue-2': '800', // use shade 600 for the <code>md-hue-2</code> class
+                'hue-3': '300', // use shade A100 for the <code>md-hue-3</code> class
+            });
+    });
--- src/NoInc/SimpleStorefront/ViewBundle/Resources/src/scripts/controllers/Security/login.js
+++ src/NoInc/SimpleStorefront/ViewBundle/Resources/src/scripts/controllers/Security/login.js
@@ -1,3 +1,4 @@
+
 app.controller('loginController', ($scope, $http) => {
     $scope.user = {
         username: null,
