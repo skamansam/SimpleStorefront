1d4e71062409bc1abf4dec507f3de3ca98f0e95e
Samuel C Tyler <skamansam@gmail.com>
2018-03-16 10:16:46 -0400

    [XXX] Fix recipe controller!

--- src/NoInc/SimpleStorefront/ApiBundle/Controller/RecipeController.php
+++ src/NoInc/SimpleStorefront/ApiBundle/Controller/RecipeController.php
@@ -1,24 +1,20 @@
 <?php
 namespace NoInc\SimpleStorefront\ApiBundle\Controller;
 
-use NoInc\SimpleStorefront\ApiBundle\Entity\Ingredient;
+use NoInc\SimpleStorefront\ApiBundle\Entity\Recipe;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class IngredientController extends Controller
+class RecipeController extends Controller
 {
     /**
      * @Route("/recipes", name="list_recipes")
      * @Method("GET")
      */
-    public function listRecipeAction(Request $request, Ingredient $ingredient)
-    {
-        $stock = $ingredient->getStock();
-        $stock++;
-        $ingredient->setStock($stock);
-
-        return $ingredient;
-    }
+    // public function listRecipeAction(Request $request, Recipe $recipe)
+    // {
+    //     return $recipe;
+    // }
 }
