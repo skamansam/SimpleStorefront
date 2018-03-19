9517eabf71ae400eedd0446f4745263ef5313250
Samuel C Tyler <skamansam@gmail.com>
2018-03-19 16:09:20 -0400

    [#24] use json-ld instead of json

--- src/NoInc/SimpleStorefront/ViewBundle/Resources/src/scripts/factories/recipeProvider.js
+++ src/NoInc/SimpleStorefront/ViewBundle/Resources/src/scripts/factories/recipeProvider.js
@@ -1,6 +1,6 @@
 app.factory('recipeProvider', ($http) => {
     const getRecipes = () =>
-        $http.get('/api/recipes').then(response => response.data);
+        $http.get('/api/recipes.jsonld').then(response => response.data['hydra:member']);
 
     const getRecipeCount = () =>
         $http.get('/api/recipes?count_only=true').then(response => response.data);
