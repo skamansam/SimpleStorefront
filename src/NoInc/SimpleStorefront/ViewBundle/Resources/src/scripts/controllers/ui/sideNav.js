8524769e32e0273767995fb1361fe01e895f21d9
Samuel C Tyler <skamansam@gmail.com>
2018-03-21 16:44:15 -0400

    [XXX] Fix bad recipes count in sideNav

--- src/NoInc/SimpleStorefront/ViewBundle/Resources/src/scripts/controllers/ui/sideNav.js
+++ src/NoInc/SimpleStorefront/ViewBundle/Resources/src/scripts/controllers/ui/sideNav.js
@@ -21,10 +21,9 @@ app.controller('sideNavController', ($scope, $state, $mdSidenav, $timeout, count
     $scope.getCounts = () => {
         countsProvider.getCounts().then((response) => {
             $scope.ingredientCount = response.ingredients || 0;
-            $scope.recipesCount = response.recipes || 0;
+            $scope.recipeCount = response.recipes || 0;
             $scope.productCount = response.products || 0;
             $scope.userCount = response.users || 0;
-            // $timeout(getCounts, 5000); // we could do so much here instead! (web sockets!)
         });
     };
 
